0dd12b68aa30127b71de35e1cc2226a9e1c00089
ByeongHyeon Choi <byeonghyeonchoe55@gmail.com>
2022-02-07 12:51:24 +0900

    Time: 92 ms (46.32%), Space: 42.4 MB (5.90%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -1,10 +1,16 @@
 var plusOne = function(digits) {
-    for(let i = digits.length - 1; i >= 0; i--) {
-        digits[i]++;
-        if(digits[i] > 9) {
-            digits[i] = 0;
-        } else return digits;
-    }
-    digits.unshift(1);
-    return digits;
+    // for(let i = digits.length - 1; i >= 0; i--) {
+    //     digits[i]++;
+    //     if(digits[i] > 9) {
+    //         digits[i] = 0;
+    //     } else return digits;
+    // }
+    // digits.unshift(1);
+    // return digits;
+    
+    const arr = (BigInt([...digits].join('')) + BigInt(1)).toString().split('');
+    const result = arr.map((el) => parseInt(el));
+    return result;
 }; 
+
+// parseInt => BigInt
