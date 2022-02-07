9e852d4b5847d9f9374365899ef4b30c5044f044
ByeongHyeon Choi <byeonghyeonchoe55@gmail.com>
2022-02-07 13:13:29 +0900

    Time: 92 ms (45.90%), Space: 42.1 MB (13.11%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,22 @@
+var merge = function(nums1, m, nums2, n) {
+//     const tmpArr = nums1.slice();
+//     let pt1 = (m + n) - 1;
+//     let pt2 = 0;
+    
+//     while(!tmpArr[pt1--]) {
+//         nums1.pop();
+//         const curNumOfNums2 = nums2[pt2++];
+//         let curIndexOfNums1 = nums1.indexOf(curNumOfNums2);
+//         if(curIndexOfNums1 === -1) {
+//             nums1.splice(nums1.indexOf(0), 0, curNumOfNums2);
+//         } else {
+//             nums1.splice(curIndexOfNums1, 0, curNumOfNums2);
+//         }
+//     }
+    
+//     return nums1;
+    nums1.splice(m, n, ...nums2);
+    nums1.sort((a, b) => a - b);
+    return nums1;
+};
+
