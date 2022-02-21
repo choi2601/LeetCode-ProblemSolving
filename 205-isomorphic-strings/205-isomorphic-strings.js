933a7c6494971fadcfe5b5237f85db3ebdf35cb5
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-02-21 12:14:23 +0900

    Time: 115 ms (44.75%), Space: 45.8 MB (11.61%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -1,15 +1,32 @@
 var isIsomorphic = function(s, t) {
-    const sH = new Map();
-    const len = s.length;
+//     const sH = new Map();
+//     const len = s.length;
     
-    for(let i = 0; i < len; i++) {
-        if(!sH.has(s[i])) {
-            sH.set(s[i], t[i]);
-        } else {
-            if(sH.get(s[i]) !== t[i]) return false;
-        }
+//     for(let i = 0; i < len; i++) {
+//         if(!sH.has(s[i])) {
+//             sH.set(s[i], t[i]);
+//         } else {
+//             if(sH.get(s[i]) !== t[i]) return false;
+//         }
+//     }
+    
+//     if(new Set([...sH.values()]).size !== sH.size) return false;
+//     else return true;
+     const obj = (str1, str2) => {
+    let result = {};
+    for (let i = 0; i < str1.length; i++) {
+      result[str1[i]] = str2[i];
     }
-    
-    if(new Set([...sH.values()]).size !== sH.size) return false;
-    else return true;
+    return result;
+  };
+
+  const doesMatch = (str1, str2, obj) => {
+    const arr = str1.split('');
+    for (let i = 0; i < str1.length; i++) {
+      arr[i] = obj[str1[i]];
+    }
+    return arr.join('') === str2 ? true : false;
+  };
+
+  return doesMatch(s, t, obj(s, t)) && doesMatch(t, s, obj(t, s));
 };
