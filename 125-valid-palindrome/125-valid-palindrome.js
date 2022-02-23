7c7c3c8dfd23d0d2eabc8b074bc80aac445b3066
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-02-23 14:05:11 +0900

    Time: 80 ms (89.17%), Space: 44.3 MB (52.39%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -5,7 +5,7 @@
 var isPalindrome = function(s) {
     const newS = s.replace(/ /gi, '').replace(/[^a-z^A-Z^0-9]/gi, '');
     
-    const lowerS = Array.from(newS).map(word => word.toLowerCase());
+    const lowerS = newS.toLowerCase();
     const len = Math.floor(lowerS.length / 2);
     
     for(let i = 0; i < len; i++) {
