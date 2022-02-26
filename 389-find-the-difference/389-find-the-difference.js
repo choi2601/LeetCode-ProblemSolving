c5789a67f7c6d148fb6e8b7b0470e3d4c1b1ac72
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-02-26 12:24:26 +0900

    Time: 82 ms (72.29%), Space: 44.7 MB (21.32%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    if(s.length === 0) return t;
+    let sH = new Map();
+    
+    for(let x of s) {
+        if(!sH.has(x)) sH.set(x, 1);
+        else sH.set(x, sH.get(x) + 1);
+    }
+    
+    let pt = 0;
+    for(let x of t) {
+        pt++;
+        if(!sH.has(x)) return x;
+        else {
+            if(sH.get(x) === 0) return x;
+            sH.set(x, sH.get(x) - 1);
+            // if(pt === t.length - 1) return x;
+        }
+    }
+};
