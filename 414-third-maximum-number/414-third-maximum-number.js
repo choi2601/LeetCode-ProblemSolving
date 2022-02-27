3bbfe2ab44320ec9313604cfd88543dacdc8b7fa
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-02-27 17:12:56 +0900

    Time: 103 ms (50.18%), Space: 44.6 MB (19.27%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.js
+++ 414-third-maximum-number/414-third-maximum-number.js
@@ -5,14 +5,11 @@
 var thirdMax = function(nums) {
     const arr = new Set(nums.sort((a, b) => b - a));
    
-    if(arr.size < 3) {
-        
-            return Array.from(arr)[0];
-        
-    }
-    let pt = 0;
-    for(let x of arr) {
-        if(pt === 2) return x;
-        pt++;
-    }
+    if(arr.size < 3) return Array.from(arr)[0];
+    else return Array.from(arr)[2];
+    // let pt = 0;
+    // for(let x of arr) {
+    //     if(pt === 2) return x;
+    //     pt++;
+    // }
 };
