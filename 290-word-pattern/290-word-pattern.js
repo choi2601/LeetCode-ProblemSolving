94d976c1b35248c9b203c4d3972dcd3611982a23
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-02-28 12:29:26 +0900

    Time: 77 ms (72.24%), Space: 42.5 MB (5.31%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    const str = s.split(' ');
+    const sH = new Map();
+    const ck1 = new Set(pattern).size;
+    const ck2 = new Set(str).size;
+    
+    if(str.length !== pattern.length || ck1 !== ck2) return false;
+    
+    for(let i = 0; i < pattern.length; i++) {
+        if(!sH.has(pattern[i])) sH.set(pattern[i], str[i])
+        else {
+            if(sH.get(pattern[i]) !== str[i]) return false;
+        }
+    }
+    return true;
+};
