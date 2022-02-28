3eaf5a8b3d0221646d0621f23de1c58e67e4bc53
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-02-28 13:23:40 +0900

    Time: 95 ms (60.13%), Space: 44.5 MB (19.53%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -4,25 +4,25 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    const frontNums = nums.slice(0, n);
-    const backNums = nums.slice(n, nums.length);
-    const shuffleNums = [];
-    
-    for(let i = 0; i < n; i++) {
-        shuffleNums.push(frontNums[i]);
-        shuffleNums.push(backNums[i]);
-    }
-    
-    return shuffleNums;
-    
-//     let pt1 = 0;
-//     let pt2 = n;
+//     const frontNums = nums.slice(0, n);
+//     const backNums = nums.slice(n, nums.length);
 //     const shuffleNums = [];
     
-//     while(pt1 !== n) {
-//         shuffleNums.push(nums[pt1++]);
-//         shuffleNums.push(nums[pt2++]);;
+//     for(let i = 0; i < n; i++) {
+//         shuffleNums.push(frontNums[i]);
+//         shuffleNums.push(backNums[i]);
 //     }
     
 //     return shuffleNums;
+    
+    let pt1 = 0;
+    let pt2 = n;
+    const shuffleNums = [];
+    
+    while(pt1 !== n) {
+        shuffleNums.push(nums[pt1++]);
+        shuffleNums.push(nums[pt2++]);;
+    }
+    
+    return shuffleNums;
 };
