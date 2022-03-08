37925a7f867c9d3d548330dafbc8621d303a622b
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-03-08 18:57:10 +0900

    Time: 111 ms (30.39%), Space: 41.8 MB (58.10%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.js
+++ 771-jewels-and-stones/771-jewels-and-stones.js
@@ -4,10 +4,13 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    let answer = 0;
-    for(let x of stones) {
-        if(jewels.includes(x)) answer++;
-    }
+//     let answer = 0;
+//     for(let x of stones) {
+//         if(jewels.includes(x)) answer++;
+//     }
+    
+//     return answer;
+    const answer = stones.split('').reduce((res, x) => jewels.includes(x) + res,0)
     
     return answer;
 };
