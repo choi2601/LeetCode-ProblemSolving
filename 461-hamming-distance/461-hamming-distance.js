d188df1c583b2b9262f2359fc56c56d9cb63d831
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-03-11 16:13:20 +0900

    Time: 55 ms (97.97%), Space: 41.9 MB (53.04%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.js
+++ 461-hamming-distance/461-hamming-distance.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
 var hammingDistance = function(x, y) {
-    return (x^y).toString(2).replace(/0/g, "").length;
+    // return (x^y).toString(2).replace(/0/g, "").length;
+    
+    let bits = x^y;
+    let diff = 0;
+    
+    while(bits) {
+        if(bits & 1) diff++;
+        bits = bits >> 1;
+    }
+    
+    return diff;
 };
