91e6829c256cedfc8e956177c345e82b28e84784
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-03-14 17:45:33 +0900

    Time: 82 ms (84.16%), Space: 45.8 MB (32.36%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,10 +3,18 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    const answer = nums.slice();
-    for(let x of nums) {
-        answer.push(x);
+//     const answer = nums.slice();
+//     for(let x of nums) {
+//         answer.push(x);
+//     }
+    
+//     return answer;
+    const len = nums.length;
+    let pt = 0;
+    while(pt < len) {
+        nums[pt + len] = nums[pt];
+        pt++;
     }
     
-    return answer;
+    return nums;
 };
