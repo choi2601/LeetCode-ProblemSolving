4ce49e39eb2df393e401b9d252c8cacb36238f59
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-03-15 12:56:00 +0900

    Time: 63 ms (93.00%), Space: 42.9 MB (10.50%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    let cnt = 0;
+    
+    while(students.length) {
+        if(!students.includes(sandwiches[0])) {
+            console.log('d');
+            students.shift();
+            cnt++;
+        } else {
+            if(sandwiches[0] === students[0]) {
+                students.shift();
+                sandwiches.shift();
+            } else {
+                const curStudent = students.shift();
+                students.push(curStudent);
+            }
+        }
+    }
+    return cnt;
+};
