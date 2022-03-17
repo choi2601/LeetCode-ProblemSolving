5c3953f185e54c236fc1d7d11899f73b810ae70f
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-03-17 15:38:24 +0900

    Time: 71 ms (73.34%), Space: 42.4 MB (11.98%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -3,13 +3,25 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-    let goodPair = 0;
+//     let goodPair = 0;
     
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[i] === nums[j]) goodPair += 1;
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//             if(nums[i] === nums[j]) goodPair += 1;
+//         }
+//     }
+    
+//     return goodPair;
+    const sH = new Map();
+    let cnt = 0;
+    
+    for(let x of nums) {
+        if(!sH.has(x)) sH.set(x, 1);
+        else {
+            cnt += sH.get(x);
+            sH.set(x, sH.get(x) + 1);
         }
     }
     
-    return goodPair;
+    return cnt;
 };
