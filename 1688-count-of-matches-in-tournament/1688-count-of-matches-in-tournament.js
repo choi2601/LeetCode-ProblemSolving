c7a4ee266b903a8ceeda529f9809b4faa52ae1e2
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-03-22 19:15:30 +0900

    Time: 90 ms (40.46%), Space: 42.4 MB (10.45%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -3,18 +3,36 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    const matched = [];
+//     const matched = [];
     
-    while(n !== 1) {
-        if(n % 2 === 0) {
-            n = n / 2;
-            matched.push(n);
-        } else {
-            n = (n - 1) / 2;
-            matched.push(n + 1);
-        }
-    }
+//     while(n !== 1) {
+//         if(n % 2 === 0) {
+//             n = n / 2;
+//             matched.push(n);
+//         } else {
+//             n = (n - 1) / 2;
+//             matched.push(n + 1);
+//         }
+//     }
     
-    if(matched.length === 0) return 0;
-    else return matched.reduce((a, b) => a + b);
+//     if(matched.length === 0) return 0;
+//     else return matched.reduce((a, b) => a + b);
+    
+  let result = 0;
+  let remain=n;
+  
+  while(remain>1){
+     if(remain%2===0){
+   result += remain/2;
+   remain= remain/2;
+ }else{
+   
+     result += (remain-1)/2;
+     remain=(remain-1)/2+1;
+   
+ }
+
+  }
+    if(!result) return 0;
+    else return result
 };
