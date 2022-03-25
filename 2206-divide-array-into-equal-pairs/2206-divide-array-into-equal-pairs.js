d147bfad0b0915a1b2cdcbf5fbfdaab29e18b31d
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-03-25 12:39:57 +0900

    Time: 84 ms (68.75%), Space: 44.8 MB (39.06%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
@@ -3,16 +3,25 @@
  * @return {boolean}
  */
 var divideArray = function(nums) {
-    const sH = new Map();
+//     const sH = new Map();
+    
+//     for(let x of nums) {
+//         if(!sH.has(x)) sH.set(x, 1);
+//         else sH.set(x, sH.get(x) + 1);
+//     }
+    
+//     for(let [_, val] of sH) {
+//         if(val % 2 !== 0) return false;
+//     }
+    
+//     return true;
+    const set = new Set();
     
     for(let x of nums) {
-        if(!sH.has(x)) sH.set(x, 1);
-        else sH.set(x, sH.get(x) + 1);
+        if(set.has(x)) set.delete(x);
+        else set.add(x);
     }
     
-    for(let [_, val] of sH) {
-        if(val % 2 !== 0) return false;
-    }
-    
-    return true;
+    if(set.size === 0) return true;
+    else return false;
 };
