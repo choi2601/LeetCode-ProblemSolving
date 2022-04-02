45f6015fd331924c2f79109e879fc303f7ed0dd3
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-04-02 12:02:28 +0900

    Time: 153 ms (44.20%), Space: 49.7 MB (68.64%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    const answer = [];
+    const n = graph.length;
+    const nodes = Array.from(Array(n), () => Array(n).fill(0));
+    const ch = Array.from({length: n}, () => 0);
+    
+    for(let i = 0; i < graph.length; i++) {
+        const route = graph[i];
+        for(let j = 0; j < route.length; j++) {
+            const path = route[j];
+            nodes[i][path] = 1;
+        }
+    }
+    
+    const tmp = [0];
+    
+    function DFS(v) {
+        if(v === n - 1) {
+            answer.push(tmp.slice());
+            return;
+        }
+        else {
+            for(let i = 0; i < n; i++) {
+                if(nodes[v][i] === 1 && ch[i] === 0) {
+                    ch[i] = 1;
+                    tmp.push(i);
+                    DFS(i);
+                    ch[i] = 0;
+                    tmp.pop();
+                }
+            }
+        }
+    }
+    
+    ch[0] = 1;
+    DFS(0);
+    
+    return answer;
+};
