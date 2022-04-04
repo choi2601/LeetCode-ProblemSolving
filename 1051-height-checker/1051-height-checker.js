0b5b13d84b60563e16f7d74852d44355aea89e79
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-04-04 15:34:11 +0900

    Time: 73 ms (67.71%), Space: 42.3 MB (52.89%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    const expected = heights.slice().sort((a, b) => a - b);
+    let cnt = 0;
+    
+    for(let i = 0; i < heights.length; i++) {
+        if(heights[i] !== expected[i]) cnt++;
+    }
+    
+    return cnt;
+};
