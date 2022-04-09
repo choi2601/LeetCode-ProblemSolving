183b862afc82f129ef93563f5970641107e78eb9
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-04-09 13:26:21 +0900

    Time: 65 ms (84.76%), Space: 42.7 MB (76.62%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -4,9 +4,28 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    const cp1 = nums1.length < nums2.length ? nums2 : nums1;
-    const cp2 = nums1.length < nums2.length ? nums1 : nums2;
-    const diff = cp1.filter(x => cp2.includes(x));
+//     const cp1 = nums1.length < nums2.length ? nums2 : nums1;
+//     const cp2 = nums1.length < nums2.length ? nums1 : nums2;
+//     const diff = cp1.filter(x => cp2.includes(x));
     
-    return [...new Set(diff)];
+//     return [...new Set(diff)];
+    const sortedNums1 = nums1.sort((a, b) => a - b);
+    const sortedNums2 = nums2.sort((a, b) => a - b);
+    
+    let pt1 = 0;
+    let pt2 = 0;
+    
+    const diff = [];
+    
+    while(pt1 < sortedNums1.length && pt2 < sortedNums2.length) {
+        if(sortedNums1[pt1] === sortedNums2[pt2]) {
+            if(!diff.includes(sortedNums1[pt1])) diff.push(sortedNums1[pt1]);
+            pt1++;
+            pt2++;
+        }
+        else if(sortedNums1[pt1] < sortedNums2[pt2]) pt1++;
+        else pt2++;
+    }
+    
+    return diff;
 };
