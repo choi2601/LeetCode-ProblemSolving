4c200fbf9e6ce7de21db78b177c2cbfb58b1df90
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2022-04-12 21:59:04 +0900

    Time: 101 ms (22.16%), Space: 44.5 MB (5.68%) - LeetHub

---
+++ 551-student-attendance-record-i/551-student-attendance-record-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+    let absentCnt = 0;
+    let lateCnt = 0;
+    let ch = [];
+    
+    for(let x of s) {
+        if(x === 'A') {
+            absentCnt++;
+            ch = [];
+        }
+        if(x === 'P') ch = [];
+        if(x === 'L') ch.push('L');
+        if(ch.length === 3) return false;
+    }
+    
+    if(absentCnt > 1) return false;
+    else return true;
+};
