d226cecc3b2d1c9def9b7e41e5129115e226b419
Byeong Hyeon <byeonghyeonchoe55@gmail.com>
2023-04-21 18:11:09 +0900

    Time: 65 ms (62.75%), Space: 42.9 MB (46.96%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxPower = function(s) {
+    let output = 1;
+    let currentPower = 1;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i+1]) {
+            currentPower++;
+        } else {
+            output = Math.max(output, currentPower);
+            currentPower = 1;
+        }
+    }
+    
+    return output;
+};
